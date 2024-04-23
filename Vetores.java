f884626835fd239a9a4cd8b8d7c33fe970a90a77
atilaF03 <144128328+atilaF03@users.noreply.github.com>
2024-04-22 21:20:06 -0300

    Create Vetores.java

---
+++ Vetores.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class app {
+    public static void main(String[] args) {
+
+        final int tam = 5;
+        char[] gabarito = { 'a', 'b', 'c', 'd', 'e' };
+        char[] respostas = new char[tam];
+        int nota = 0;
+       
+        Scanner scanner  = new Scanner(System.in);
+
+        for(int i= 0;i<tam;i++){
+          System.out.printf("informe a nota é:%d",i);
+          respostas[i]=scanner.nextLine().charAt(0);
+        }
+        
+        for(int i= 0;i<tam;i++){
+            if (respostas[i]== gabarito[i]) {
+                nota++;
+            }
+        }
+        System.out.println("os acertos dos aluno:"+nota);
+
+    }
+}
